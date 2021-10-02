3dc63b16ab86ee8708c20ffd87f4dd5668f28229
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2021-10-02 11:13:59 -0500

    added javaScript form validation

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
-// alert('hello world!');
+
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    checkEmail();
+});
+
+function checkEmail() {
+    const emailValue = email.value;
+    if (!isEmail(emailValue)) {
+        form.classList = 'error';
+    }
+}
+
+function isEmail(email) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+}
