beb7cd737e34e856d45b7e721ff99f6c18e34b8c
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2021-10-03 10:46:18 -0500

    updated js and readme

--- app/app.js
+++ app/app.js
@@ -4,16 +4,27 @@ const email = document.getElementById('email');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    checkEmail();
+    checkEmailValue();
 });
 
-function checkEmail() {
+function checkEmailValue() {
     const emailValue = email.value;
-    if (!isEmail(emailValue)) {
+    const small = document.querySelector('small');
+    let result = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailValue);
+    
+    if (emailValue === '') {
+        message('Opps! Please add your email');
+        form.classList = 'error';
+    } else if (result === false) {
+        message('Opps! Please provide a valid email');
+        form.classList = 'error';
+    } else if (result === true) {
+        message('Success!');
         form.classList = 'error';
     }
 }
 
-function isEmail(email) {
-    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+function message(message) {
+    const small = document.querySelector('small');
+    small.innerText = message;
 }
